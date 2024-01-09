82c15c9b055ef9326b78ee3361dd14a0b951f9f9
25jao <joao.paulo.sgcosta@gmail.com>
2024-01-09 01:13:48 -0300

    balanceamento

--- Hard Magic/Assets/boss.cs
+++ Hard Magic/Assets/boss.cs
@@ -37,173 +37,179 @@ public class boss : MonoBehaviour
         spawnaFantasmacdTimer = spawnaFantasmaCd;
         healTotem = Resources.Load<GameObject>("Pedraheal");
         healcdTimer = healcdTime;
+        arenamaxx += transform.position.x;
+        arenaminx += transform.position.x;
+        arenamaxy += transform.position.z;
+        arenaminy += transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(andando){
-            //Debug.Log("aaaaaaaa");
-            if(Vector3.Distance(walkTarget.transform.position, new Vector3(transform.position.x, 0, transform.position.z)) < 0.1f){
-                //if(nma.isStopped){
-                //Debug.Log("ccccc");
-                andando = false;
-                animator.SetBool("andar", false);
-            }
-        }
-
-        if(cdEntreAcaoTimer < cdEntreAcao){
-            cdEntreAcaoTimer += Time.deltaTime;
-        }
-
-        if(!andando && !atacando && cdEntreAcaoTimer > cdEntreAcao){
-            int acao = Random.Range(2, 4);
-            Debug.Log(acao);
-            if(acao == 0 && raioCdTimer >= raiocd){
-                atacando = true;
-                atiraRaio = true;
-                animator.SetBool("raio", true);
-                alvo = GameObject.Instantiate(alvoprefab);
-                alvo.transform.position = transform.position;
-                alvo.GetComponent<magia>().spawnar(dmg:danoRaio, team:1, dt:100, continuo:true);
-            }
-            if(acao == 1 && levantaParedeCdtimer >= levantaParedeCd){
-                atacando = true;
-                subindoParede = true;
-                animator.SetBool("levantar", true);
-                paredes[0] = Instantiate(puddlePrefab);
-                paredes[0].transform.position = player.transform.position + new Vector3(0, 0.5f, 0);
-                //Destroy(paredes[0], paredeCastTime);
-                for(int i = 1; i < nparedes; i++){
-                    float x = Random.Range(arenaminx, arenamaxx);
-                    float y = Random.Range(arenaminy, arenamaxy);
-                    paredes[i] = Instantiate(puddlePrefab);
-                    paredes[i].transform.position = new Vector3(x, 0.5f, y);
-                    //Destroy(paredes[i], paredeCastTime + 1f);
+        if(Vector3.Distance(player.transform.position, transform.position) < 15f){
+            if(andando){
+                //Debug.Log("aaaaaaaa");
+                if(Vector3.Distance(walkTarget.transform.position, new Vector3(transform.position.x, 0, transform.position.z)) < 0.1f){
+                    //if(nma.isStopped){
+                    //Debug.Log("ccccc");
+                    andando = false;
+                    animator.SetBool("andar", false);
                 }
             }
-            if(acao == 2 && spawnaFantasmacdTimer >= spawnaFantasmaCd){
-                atacando = true;
-                spawnandoBixo = true;
-                animator.SetBool("paitaon", true);
-            }
-            if(acao == 3 && healcdTimer >= healcdTime){
-                atacando = true;
-                healando = true;
-                animator.SetBool("suba", true);
-            }
-        }
 
-        if(healando){
-            if(castTimer < healCastTime){
-                castTimer += Time.deltaTime;
-            }else{
-                for(int i = 0; i < npedras; i++){
-                    GameObject pedra = Instantiate(healTotem);
-                    achaLugarPraPedra(pedra);
-                    Destroy(pedra, HealDur);
-                    animator.SetBool("suba", false);
-                    durtimer = 0;
-                    healando = false;
-                    atacando = false;
-                    castTimer = 0;
-                    cdEntreAcaoTimer = 0;
-                    healcdTimer = 0;
-                }
+            if(cdEntreAcaoTimer < cdEntreAcao){
+                cdEntreAcaoTimer += Time.deltaTime;
             }
-        }
 
-        if(spawnandoBixo){
-            if(castTimer < spawnaBixoCastTime){
-                castTimer += Time.deltaTime;
-            }else{
-                if(durtimer == 0){
-                    GameObject go = Instantiate(ghostprefab);
-                    go.transform.position = transform.position;
+            if(!andando && !atacando && cdEntreAcaoTimer > cdEntreAcao){
+                int acao = Random.Range(0, 4);
+                Debug.Log(acao);
+                if(acao == 0 && raioCdTimer >= raiocd){
+                    atacando = true;
+                    atiraRaio = true;
+                    animator.SetBool("raio", true);
+                    alvo = GameObject.Instantiate(alvoprefab);
+                    alvo.transform.position = transform.position;
+                    alvo.GetComponent<magia>().spawnar(dmg:danoRaio, team:1, dt:100, continuo:true);
                 }
-                fantasmaTimer += Time.deltaTime;
-                if(fantasmaTimer >= fantasmaAtkDur / nghost){
-                    GameObject go = Instantiate(ghostprefab);
-                    go.transform.position = transform.position;
-                    fantasmaTimer = 0;
-                }
-                durtimer += Time.deltaTime;
-                if(durtimer > fantasmaAtkDur){
-                    animator.SetBool("paitaon", false);
-                    durtimer = 0;
-                    atacando = false;
-                    castTimer = 0;
-                    cdEntreAcaoTimer = 0;
-                    spawnaFantasmacdTimer = 0;
-                    fantasmaTimer = 0;
-                    spawnandoBixo = false;
-                }
-            }
-        }
-
-        if(subindoParede){
-            if(castTimer < paredeCastTime){
-                castTimer += Time.deltaTime;
-            }else{
-                if(durtimer == 0){
-                    animator.SetBool("parede2", true);
-                    animator.SetBool("levantar", false);
-                    for(int i = 0; i < nparedes; i++){
-                        Vector3 pos = paredes[i].transform.position;
-                        Destroy(paredes[i]);
-                        GameObject go = Instantiate(areaDMG);
-                        go.transform.position = pos;
-                        go.GetComponent<magia>().spawnar(dmg:danoParede, team:1, dt:0.25f);
-                        paredes[i] = Instantiate(pilarPrefab);
-                        paredes[i].transform.position = pos - Vector3.up * 10f;
-                        Destroy(paredes[i], paredeDur);
+                if(acao == 1 && levantaParedeCdtimer >= levantaParedeCd){
+                    atacando = true;
+                    subindoParede = true;
+                    animator.SetBool("levantar", true);
+                    paredes[0] = Instantiate(puddlePrefab);
+                    paredes[0].transform.position = player.transform.position + new Vector3(0, 0.5f, 0);
+                    //Destroy(paredes[0], paredeCastTime);
+                    for(int i = 1; i < nparedes; i++){
+                        float x = Random.Range(arenaminx, arenamaxx);
+                        float y = Random.Range(arenaminy, arenamaxy);
+                        paredes[i] = Instantiate(puddlePrefab);
+                        paredes[i].transform.position = new Vector3(x, 0.5f, y);
+                        //Destroy(paredes[i], paredeCastTime + 1f);
                     }
                 }
-                for(int i = 0; i < nparedes; i++){
-                    paredes[i].transform.position += new Vector3(0, 10/paredeAtkDur * Time.deltaTime, 0);
+                if(acao == 2 && spawnaFantasmacdTimer >= spawnaFantasmaCd){
+                    atacando = true;
+                    spawnandoBixo = true;
+                    animator.SetBool("paitaon", true);
                 }
-                durtimer += Time.deltaTime;
-                if(durtimer > paredeAtkDur){
-                    animator.SetBool("parede2", false);
-                    durtimer = 0;
-                    subindoParede = false;
-                    atacando = false;
-                    castTimer = 0;
-                    cdEntreAcaoTimer = 0;
-                    levantaParedeCdtimer = 0;
+                if(acao == 3 && healcdTimer >= healcdTime){
+                    atacando = true;
+                    healando = true;
+                    animator.SetBool("suba", true);
                 }
             }
-        }
 
-        if(atiraRaio){
-            //alvo.transform.position += (alvo.transform.position - player.transform.position) * targetSpeed;
-            alvo.transform.position = Vector3.MoveTowards(alvo.transform.position, player.transform.position, targetSpeed * Time.deltaTime);
-            transform.forward = alvo.transform.position - transform.position;
-            if(castTimer < raioCastTime){
-                castTimer += Time.deltaTime;
-                
-            }else{
-                if(durtimer == 0){
-                    raio = Instantiate(raioprefab);
+            if(healando){
+                if(castTimer < healCastTime){
+                    castTimer += Time.deltaTime;
+                }else{
+                    for(int i = 0; i < npedras; i++){
+                        GameObject pedra = Instantiate(healTotem);
+                        achaLugarPraPedra(pedra);
+                        Destroy(pedra, HealDur);
+                        animator.SetBool("suba", false);
+                        durtimer = 0;
+                        healando = false;
+                        atacando = false;
+                        castTimer = 0;
+                        cdEntreAcaoTimer = 0;
+                        healcdTimer = 0;
+                    }
+                }
+            }
+
+            if(spawnandoBixo){
+                if(castTimer < spawnaBixoCastTime){
+                    castTimer += Time.deltaTime;
+                }else{
+                    if(durtimer == 0){
+                        GameObject go = Instantiate(ghostprefab);
+                        go.transform.position = transform.position;
+                    }
+                    fantasmaTimer += Time.deltaTime;
+                    if(fantasmaTimer >= fantasmaAtkDur / nghost){
+                        GameObject go = Instantiate(ghostprefab);
+                        go.transform.position = transform.position;
+                        fantasmaTimer = 0;
+                    }
+                    durtimer += Time.deltaTime;
+                    if(durtimer > fantasmaAtkDur){
+                        animator.SetBool("paitaon", false);
+                        durtimer = 0;
+                        atacando = false;
+                        castTimer = 0;
+                        cdEntreAcaoTimer = 0;
+                        spawnaFantasmacdTimer = 0;
+                        fantasmaTimer = 0;
+                        spawnandoBixo = false;
+                    }
+                }
+            }
+
+            if(subindoParede){
+                if(castTimer < paredeCastTime){
+                    castTimer += Time.deltaTime;
+                }else{
+                    if(durtimer == 0){
+                        animator.SetBool("parede2", true);
+                        animator.SetBool("levantar", false);
+                        for(int i = 0; i < nparedes; i++){
+                            Vector3 pos = paredes[i].transform.position;
+                            Destroy(paredes[i]);
+                            GameObject go = Instantiate(areaDMG);
+                            go.transform.position = pos;
+                            go.GetComponent<magia>().spawnar(dmg:danoParede, team:1, dt:0.25f);
+                            paredes[i] = Instantiate(pilarPrefab);
+                            paredes[i].transform.position = pos - Vector3.up * 10f;
+                            Destroy(paredes[i], paredeDur);
+                        }
+                    }
+                    for(int i = 0; i < nparedes; i++){
+                        paredes[i].transform.position += new Vector3(0, 10/paredeAtkDur * Time.deltaTime, 0);
+                    }
+                    durtimer += Time.deltaTime;
+                    if(durtimer > paredeAtkDur){
+                        animator.SetBool("parede2", false);
+                        durtimer = 0;
+                        subindoParede = false;
+                        atacando = false;
+                        castTimer = 0;
+                        cdEntreAcaoTimer = 0;
+                        levantaParedeCdtimer = 0;
+                    }
+                }
+            }
+
+            if(atiraRaio){
+                //alvo.transform.position += (alvo.transform.position - player.transform.position) * targetSpeed;
+                alvo.transform.position = Vector3.MoveTowards(alvo.transform.position, player.transform.position, targetSpeed * Time.deltaTime);
+                transform.forward = alvo.transform.position - transform.position;
+                if(castTimer < raioCastTime){
+                    castTimer += Time.deltaTime;
+                    
+                }else{
+                    if(durtimer == 0){
+                        raio = Instantiate(raioprefab);
+                        raio.transform.position = maozinha.transform.position;
+                        raio.transform.forward = alvo.transform.position - raio.transform.position;
+
+                    }
+                    durtimer += Time.deltaTime;
+                    raio.transform.localScale = Vector3.Distance(maozinha.transform.position, alvo.transform.position) / 3 * new Vector3(1,1,1);
                     raio.transform.position = maozinha.transform.position;
                     raio.transform.forward = alvo.transform.position - raio.transform.position;
-
-                }
-                durtimer += Time.deltaTime;
-                raio.transform.localScale = Vector3.Distance(maozinha.transform.position, alvo.transform.position) / 3 * new Vector3(1,1,1);
-                raio.transform.position = maozinha.transform.position;
-                raio.transform.forward = alvo.transform.position - raio.transform.position;
-                if(durtimer >= raioatkDuracao){
-                    Destroy(raio);
-                    Destroy(alvo);
-                    animator.SetBool("raio", false);
-                    atacando = false;
-                    atiraRaio = false;
-                    durtimer = 0;
-                    castTimer = 0;
-                    raioCdTimer = 0;
-                    cdEntreAcaoTimer = 0;
-                    //levantaParedeCdtimer = 0;
+                    if(durtimer >= raioatkDuracao){
+                        Destroy(raio);
+                        Destroy(alvo);
+                        animator.SetBool("raio", false);
+                        atacando = false;
+                        atiraRaio = false;
+                        durtimer = 0;
+                        castTimer = 0;
+                        raioCdTimer = 0;
+                        cdEntreAcaoTimer = 0;
+                        //levantaParedeCdtimer = 0;
+                    }
                 }
             }
         }
